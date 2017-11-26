5d69b2ea755eddc75455ad0b088c06d94c9b950d
Derrick Hu <huderrick@berkeley.edu>
2017-11-25 19:52:52 -0800

    todoList

---
+++ javascript/dom/todoList/todoList.js
@@ -0,0 +1,15 @@
+var list = document.querySelectorAll("#hover") ;
+
+for (var i = 0; i < list.length ; i++) {
+	list[i].addEventListener("mouseover", function() {
+	this.classList.add("selected")
+})
+
+	list[i].addEventListener("mouseout", function() {
+	this.classList.remove("selected")
+})
+
+	list[i].addEventListener("click", function() {
+	this.classList.toggle("clicked")
+})
+}
